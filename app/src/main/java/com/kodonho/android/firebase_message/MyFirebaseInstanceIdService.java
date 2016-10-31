a7cefec03fbd080e69e1b5dbe91da2fea350bc41
javafa <nigne2016@gmail.com>
2016-10-31 17:03:06 +0900

    최초등록

---
+++ app/src/main/java/com/kodonho/android/firebase_message/MyFirebaseInstanceIdService.java
@@ -0,0 +1,24 @@
+package com.kodonho.android.firebase_message;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseInstanceId";
+    public MyFirebaseInstanceIdService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.i(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
